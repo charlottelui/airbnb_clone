31a7ea101639514d5b5ae32a5bdb47d3a5251058
talia wolf <taliamwolf@gmail.com>
2020-02-16 14:11:41 +0200

    items controller done

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,50 @@
+class ItemsController < ApplicationController
+  before_action :set_item, [:show, :edit, :update, :destroy]
+  def index
+    @items = Item.all
+  end
+
+  def show
+
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to item_path(@item), notice: 'item was successfully added.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item), notice: 'item was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to items_path, notice: "item was successfully deleted"
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit(:title, :description, :price, :photo)
+  end
+end
