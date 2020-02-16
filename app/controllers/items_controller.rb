7cb98b2097f72e1a4369434602aed806bb3360df
talia wolf <taliamwolf@gmail.com>
2020-02-16 15:08:26 +0200

    added foreign key to items and all mvc done for items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -45,6 +45,7 @@ class ItemsController < ApplicationController
   end
 
   def item_params
-    params.require(:item).permit(:title, :description, :price, :photo)
+    params.require(:item).permit(:title, :description, :price)
+    # add :price
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,6 @@ class Item < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true
   validates :price, numericality: { only_integer: true }
-  has_one_attached :photo
+  belongs_to :user
+  # has_one_attached :photo
 end
