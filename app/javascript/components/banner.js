3e5b66867ba37373590771bb2cbe5174d0ec677f
Angel Shamieh <angellshamieh5@gmail.com>
2020-02-18 19:50:41 +0200

    adding typed js to homepage

---
+++ app/javascript/components/banner.js
@@ -0,0 +1,12 @@
+import Typed from 'typed.js';
+
+const loadDynamicBannerText = () => {
+  new Typed('#banner-typed-text', {
+    strings: ["Reduce  Reuse  Recycle 🌍"],
+    typeSpeed: 50,
+    loop: true
+  });
+}
+
+export { loadDynamicBannerText };
+
