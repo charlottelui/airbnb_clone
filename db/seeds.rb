6db6da32883cb4b9fee7b1ac299e1bf46f4ce083
talia wolf <taliamwolf@gmail.com>
2020-02-19 16:04:40 +0200

    carousel don on show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,16 +18,16 @@ angel = User.create!(first_name: 'Angel', email: 'angel@angel.com', password: '1
 
 puts 'creating items'
 
-Item.create!(title: 'Sandwich', description: 'Half eaten BLT sandwich from yesterday', price: 5, user: charlotte)
-Item.create(title: 'Harry Potter vol. 3', description: 'Ripped out a few pages in chapter 6 but still readable', price: 10, user: charlotte)
-Item.create(title: 'iPhone 4s', description: 'The phone does not turn on but you can use it as a mirror', price: 30, user: charlotte)
+Item.create!(title: 'Half eaten sandwich', description: 'Half eaten BLT sandwich from yesterday. This sandwich really was very tasty but I asked the restaurant to leave out the tomatoes and they sent it with the tomatoes anyway. I did not want to throw away the sandwich because I only had one bite.', price: 5, user: charlotte)
+Item.create(title: 'Harry Potter and the Chamber of Secrets', description: 'I ripped out a few chapters because I wanted to read this book on vacation but the whole book did not fit in my carry on. The book is still readble though and I am sure you can find the other chapters somewhere online too.', price: 10, user: charlotte)
+Item.create(title: 'iPhone 4s', description: 'The phone does not turn on but you can use it as a mirror. I also have a cool case that comes with the phone as well. I bought the iPhone X So I do not really need this one anymore.', price: 30, user: charlotte)
 
-Item.create(title: 'coffee', description: 'tons of caffeine to get you through the day. Just too much milk', price: 1, user: talia)
-Item.create(title: 'House plant', description: 'forgot to water it for a few weeks but the pot is so pretty', price: 15, user: talia)
-Item.create(title: 'watch', description: 'always says it is 3 pm', price: 20, user: talia)
+Item.create(title: 'Half a cup of coffee', description: 'I went to sleep at 5 am and then made this coffee at work. I must have been really tired because I accidentally put salt in the coffee instead of sugar. I had a couple of sips and could not finish it.', price: 1, user: talia)
+Item.create(title: 'Dead house plant', description: 'I went on vacation for a couple of weeks and my little brother forgot to water it while I was away. The pot is really pretty though!.', price: 15, user: talia)
+Item.create(title: 'Broken watch', description: 'The watch is stuck and it always says that the time is 3 pm. It is a nice watch and I always get tons of compliments on it. It matches every outfit!', price: 20, user: talia)
 
-Item.create(title: 'Gucci', description: 'Pees on everything but otherwise very cute', price: 5000, user: angel)
-Item.create(title: 'New York Times 1997', description: 'A bit outdated but a great read', price: 100, user: angel)
+Item.create(title: 'Gucci', description: 'Pees on everything but otherwise very cute. He is a lot of responsibility but he is fun to play with!', price: 5000, user: angel)
+Item.create(title: 'New York Times 1997', description: 'A bit outdated but a great read.', price: 100, user: angel)
 Item.create(title: 'whiteboard', description: 'I am too lazy to buy whiteboard cleaner. Comes with a free marker', price: 22, user: angel)
 
 puts "you have #{User.count} users & #{Item.count} items"
